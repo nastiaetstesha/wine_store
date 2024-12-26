4bfa003f796720c8ca5f09c992a48d35da69e767
Avakova Anastasiia <avakova.ast@gmail.com>
2024-12-26 23:59:18 +0300

    added reqs and env

--- main.py
+++ main.py
@@ -2,31 +2,32 @@ import collections
 import datetime as dt
 import sys
 import pandas
+import os
+
+from dotenv import load_dotenv
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from pathlib import Path
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
 def get_age_since(establish_year=1920):
-    # вычисление возраста с момента основания
     age = dt.date.today().year - establish_year
 
-    # Определяем правильное склонение для слова "год"
-    if 11 <= age % 100 <= 19:  # Проверка для чисел, оканчивающихся на 11-19
-        years_name =  "лет"
-    elif age % 10 == 1:  # Если заканчивается на 1
-        years_name =  "год"
-    elif 2 <= age % 10 <= 4:  # Если заканчивается на 2, 3, 4
-        years_name =  "года"
-    else:  # Во всех остальных случаях "лет"
+    if 11 <= age % 100 <= 19:
+        years_name = "лет"
+    elif age % 10 == 1:
+        years_name = "год"
+    elif 2 <= age % 10 <= 4:
+        years_name = "года"
+    else:
         years_name = "лет"
     return f"{age} {years_name}"
 
 
 def main():
-    path_to_wines = "/Users/egorsemin/Practice/wine_store/wine3.xlsx"
-
-    path_to_wines = Path(path_to_wines)
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    path_to_wines = Path(os.environ.get('PATH_TO_WINES'))
     if not path_to_wines.exists():
         sys.exit("Файл с винами не существует")
 
@@ -48,8 +49,10 @@ def main():
     sorted_categories = sorted(wines_categorised.keys())
 
     rendered_page = template.render(
-    age=get_age_since(establish_year=1920),
-    wines_categorised={category: wines_categorised[category] for category in sorted_categories})
+        age=get_age_since(establish_year=1920),
+        wines_categorised={
+            category: wines_categorised[category]
+            for category in sorted_categories})
 
     with open("index.html", "w", encoding="utf8") as file:
         file.write(rendered_page)
